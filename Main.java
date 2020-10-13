1d899c678fa0a69c3f28383fad3e56662f0e600b
ellag7121 <ellag7121@wrdsb.ca>
2020-10-13 13:59:13 +0000

    finished a7q4!

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
 /**
- *
- * @author 
+ * gets a number, tells the user the last digit of the number
+ * @author Graham Ellacott
  */
 public class Main {
+
+  //the method that will find the last digit in a given number
+  public static int lastDigit(int number){
+    int remainder = number % 10; //gets the reaminder of the given number and 10 (gets the last digit)
+    return remainder; //returns the last digit
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //make a loop to check a bunch of numbers
+    for(int i = 0; i < 100; i += 7){
+      int num = lastDigit(i); //store the last digit in a variable
+      System.out.println(num); //print it to the screen
+    }
     
   }
 }
